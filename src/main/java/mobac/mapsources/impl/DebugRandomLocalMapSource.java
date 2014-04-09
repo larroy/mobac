76bb5134fd94cd0058444ea365d6be3b8ea7d200
r_x <r_x@a036c9c4-4c23-4c51-82c3-8fdb5c987102>
2014-04-09 12:26:10 +0000

    Another debug map source
    git-svn-id: svn://svn.code.sf.net/p/mobac/code/trunk/MOBAC@2193 a036c9c4-4c23-4c51-82c3-8fdb5c987102

--- src/main/java/mobac/mapsources/impl/DebugRandomLocalMapSource.java
+++ src/main/java/mobac/mapsources/impl/DebugRandomLocalMapSource.java
@@ -16,58 +16,13 @@
  ******************************************************************************/
 package mobac.mapsources.impl;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.security.SecureRandom;
-
-import javax.imageio.ImageIO;
-
-import mobac.exceptions.UnrecoverableDownloadException;
-import mobac.gui.mapview.PreviewMap;
-import mobac.mapsources.mapspace.MercatorPower2MapSpace;
 import mobac.program.interfaces.FileBasedMapSource;
-import mobac.program.interfaces.MapSource;
-import mobac.program.interfaces.MapSpace;
-import mobac.program.model.MapSourceLoaderInfo;
-import mobac.program.model.TileImageType;
 
 /**
  * A {@link FileBasedMapSource} for debugging and testing purposes
  */
-public class DebugRandomLocalMapSource implements MapSource, FileBasedMapSource {
+public class DebugRandomLocalMapSource extends DebugRandomMapSource implements FileBasedMapSource {
 
-	BufferedImage image = null;
-	byte[] imageData = null;
-
-	public Color getBackgroundColor() {
-		return Color.BLACK;
-	}
-
-	public MapSpace getMapSpace() {
-		return MercatorPower2MapSpace.INSTANCE_256;
-	}
-
-	public int getMaxZoom() {
-		return PreviewMap.MAX_ZOOM;
-	}
-
-	public int getMinZoom() {
-		return 0;
-	}
-
-	@Override
-	public TileImageType getTileImageType() {
-		return TileImageType.PNG;
-	}
-
-	public void initialize() {
-	}
-
-	public void reinitialize() {
-	}
 
 	@Override
 	public String getName() {
@@ -79,59 +34,4 @@ public class DebugRandomLocalMapSource implements MapSource, FileBasedMapSource 
 		return "Debug Random (local)";
 	}
 
-	public byte[] getTileData(int zoom, int x, int y, LoadMethod loadMethod) throws IOException,
-			UnrecoverableDownloadException, InterruptedException {
-		if (imageData != null)
-			return imageData;
-		synchronized (this) {
-			if (imageData != null)
-				return imageData;
-			ByteArrayOutputStream buf = new ByteArrayOutputStream(16000);
-			BufferedImage image = getTileImage(zoom, x, y, loadMethod);
-			if (image == null)
-				return null;
-			ImageIO.write(image, "png", buf);
-			imageData = buf.toByteArray();
-			return imageData;
-		}
-	}
-
-	public BufferedImage getTileImage(int zoom, int x, int y, LoadMethod loadMethod) throws IOException,
-			UnrecoverableDownloadException, InterruptedException {
-		if (image != null)
-			return image;
-		synchronized (this) {
-			if (image != null)
-				return image;
-			BufferedImage image = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
-			Graphics2D g2 = image.createGraphics();
-			SecureRandom rnd = new SecureRandom();
-			try {
-				g2.setColor(Color.WHITE);
-				g2.fillRect(0, 0, 255, 255);
-				for (int i = 0; i < 100; i++) {
-					g2.setColor(new Color(rnd.nextInt()));
-					int x1 = rnd.nextInt(256);
-					int y1 = rnd.nextInt(256);
-					int x2 = rnd.nextInt(256);
-					int y2 = rnd.nextInt(256);
-					g2.drawLine(x1, y1, x2, y2);
-				}
-				g2.setColor(Color.RED);
-				this.image = image;
-				return image;
-			} finally {
-				g2.dispose();
-			}
-		}
-	}
-
-	@Override
-	public MapSourceLoaderInfo getLoaderInfo() {
-		return null;
-	}
-
-	@Override
-	public void setLoaderInfo(MapSourceLoaderInfo loaderInfo) {
-	}
 }
--- src/main/java/mobac/mapsources/impl/DebugRandomMapSource.java
+++ src/main/java/mobac/mapsources/impl/DebugRandomMapSource.java
@@ -17,41 +17,30 @@
 package mobac.mapsources.impl;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.awt.image.IndexColorModel;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.zip.Deflater;
+import java.security.SecureRandom;
+
+import javax.imageio.ImageIO;
 
 import mobac.exceptions.UnrecoverableDownloadException;
 import mobac.gui.mapview.PreviewMap;
 import mobac.mapsources.mapspace.MercatorPower2MapSpace;
+import mobac.program.interfaces.FileBasedMapSource;
 import mobac.program.interfaces.MapSource;
 import mobac.program.interfaces.MapSpace;
 import mobac.program.model.MapSourceLoaderInfo;
 import mobac.program.model.TileImageType;
-import mobac.utilities.imageio.Png4BitWriter;
 
-public class DebugMapSource implements MapSource {
+/**
+ * A {@link FileBasedMapSource} for debugging and testing purposes
+ */
+public class DebugRandomMapSource implements MapSource {
 
-	private int pngCompressionLevel = Deflater.NO_COMPRESSION;
-
-	private static final byte[] COLORS = { 0,//
-			(byte) 0xff, (byte) 0xff, (byte) 0xff, // white
-			(byte) 0xcc, (byte) 0xcc, (byte) 0xcc // light gray
-	};
-
-	private static final IndexColorModel COLORMODEL = new IndexColorModel(8, 2, COLORS, 1, false);
-
-	private Color COLOR_BG = new Color(COLORS[1] & 0xFF, COLORS[2] & 0xFF, COLORS[3] & 0xFF);
-	private Color COLOR_VG = new Color(COLORS[4] & 0xFF, COLORS[5] & 0xFF, COLORS[6] & 0xFF);
-
-	private static final Font FONT_LARGE = new Font("Sans Serif", Font.BOLD, 30);
-
-	public DebugMapSource() {
-	}
+	BufferedImage image = null;
+	byte[] imageData = null;
 
 	public Color getBackgroundColor() {
 		return Color.BLACK;
@@ -69,56 +58,80 @@ public class DebugMapSource implements MapSource {
 		return 0;
 	}
 
-	public String getName() {
-		return "Debug";
-	}
-
-	public byte[] getTileData(int zoom, int x, int y, LoadMethod loadMethod) throws IOException,
-			UnrecoverableDownloadException, InterruptedException {
-		ByteArrayOutputStream buf = new ByteArrayOutputStream(16000);
-		// ImageIO.write(getTileImage(zoom, x, y, LoadMethod.DEFAULT), "png", buf);
-		String pngMetaText = String.format("zoom=%d x=%d y=%d", zoom, x, y);
-		Png4BitWriter.writeImage(buf, getTileImage(zoom, x, y, loadMethod), pngCompressionLevel, pngMetaText);
-		return buf.toByteArray();
-	}
-
-	public BufferedImage getTileImage(int zoom, int x, int y, LoadMethod loadMethod) throws IOException,
-			UnrecoverableDownloadException, InterruptedException {
-		BufferedImage image = new BufferedImage(256, 256, BufferedImage.TYPE_BYTE_INDEXED, COLORMODEL);
-		Graphics2D g2 = image.createGraphics();
-		try {
-			g2.setColor(COLOR_BG);
-			g2.fillRect(0, 0, 255, 255);
-			g2.setColor(COLOR_VG);
-			g2.drawRect(0, 0, 255, 255);
-			g2.drawRect(1, 1, 254, 254);
-			g2.drawLine(0, 0, 255, 255);
-			g2.drawLine(255, 0, 0, 255);
-			g2.setFont(FONT_LARGE);
-			g2.drawString("x: " + x, 8, 40);
-			g2.drawString("y: " + y, 8, 75);
-			g2.drawString("z: " + zoom, 8, 110);
-			return image;
-		} finally {
-			g2.dispose();
-		}
-	}
-
+	@Override
 	public TileImageType getTileImageType() {
 		return TileImageType.PNG;
 	}
 
-	public MapSourceLoaderInfo getLoaderInfo() {
-		return null;
+	public void initialize() {
 	}
 
-	public void setLoaderInfo(MapSourceLoaderInfo loaderInfo) {
-		throw new RuntimeException("LoaderInfo can not be set");
+	public void reinitialize() {
+	}
+
+	@Override
+	public String getName() {
+		return "DebugRandom";
 	}
 
 	@Override
 	public String toString() {
-		return getName();
+		return "Debug Random";
 	}
 
+	public byte[] getTileData(int zoom, int x, int y, LoadMethod loadMethod) throws IOException,
+			UnrecoverableDownloadException, InterruptedException {
+		if (imageData != null)
+			return imageData;
+		synchronized (this) {
+			if (imageData != null)
+				return imageData;
+			ByteArrayOutputStream buf = new ByteArrayOutputStream(16000);
+			BufferedImage image = getTileImage(zoom, x, y, loadMethod);
+			if (image == null)
+				return null;
+			ImageIO.write(image, "png", buf);
+			imageData = buf.toByteArray();
+			return imageData;
+		}
+	}
+
+	public BufferedImage getTileImage(int zoom, int x, int y, LoadMethod loadMethod) throws IOException,
+			UnrecoverableDownloadException, InterruptedException {
+		if (image != null)
+			return image;
+		synchronized (this) {
+			if (image != null)
+				return image;
+			BufferedImage image = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g2 = image.createGraphics();
+			SecureRandom rnd = new SecureRandom();
+			try {
+				g2.setColor(Color.WHITE);
+				g2.fillRect(0, 0, 255, 255);
+				for (int i = 0; i < 100; i++) {
+					g2.setColor(new Color(rnd.nextInt()));
+					int x1 = rnd.nextInt(256);
+					int y1 = rnd.nextInt(256);
+					int x2 = rnd.nextInt(256);
+					int y2 = rnd.nextInt(256);
+					g2.drawLine(x1, y1, x2, y2);
+				}
+				g2.setColor(Color.RED);
+				this.image = image;
+				return image;
+			} finally {
+				g2.dispose();
+			}
+		}
+	}
+
+	@Override
+	public MapSourceLoaderInfo getLoaderInfo() {
+		return null;
+	}
+
+	@Override
+	public void setLoaderInfo(MapSourceLoaderInfo loaderInfo) {
+	}
 }
